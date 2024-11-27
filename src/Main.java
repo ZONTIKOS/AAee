2e21071c8965175a9415d6f23945605b6369368a
ZONTIKOS <you@example.com>
2024-11-27 21:10:51 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        //git init
+//        git add README.md
+//        git commit -m "first commit"
+//        git remote add origin https://github.com/ZONTIKOS/AAee.git
+//        git push -u origin main
+    }
+}
